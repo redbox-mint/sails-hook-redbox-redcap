da5eb3a2bb4de2a1dab4afe1df136539a89025c9
Andrew Brazzatti <andrew@redboxresearchdata.com.au>
2026-07-11 23:55:00 +0930

    test: verify registered REDCap asset URL

--- test/integration/hook-registration.test.ts
+++ test/integration/hook-registration.test.ts
@@ -7,6 +7,6 @@ describe('REDCap hook integration', function () {
     const hookModule = require('@researchdatabox/sails-hook-redbox-redcap');
     expect(hookModule.registerRedboxFormConfigs()).to.have.property('redcap-1.0-draft');
     expect(hookModule.registerRedboxConfig().recordtype).to.have.property('redcap');
-    expect(require('node:fs').existsSync(require('node:path').join(process.cwd(), 'assets/angular/redcap/browser/assets/images/logo.png'))).to.equal(true);
+    expect(hookModule.registerRedboxConfig().workspacetype.redcap.logo).to.equal('/angular/redcap/assets/images/logo.png');
   });
 });
